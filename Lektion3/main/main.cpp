e545b1c5cda62c2dba0a4767d2657630ee22f8c0
Per Thöyrä <per.thoyra@outlook.com>
2024-05-21 15:03:16 +0200

    Commented code

--- Lektion3/main/main.cpp
+++ Lektion3/main/main.cpp
@@ -3,11 +3,11 @@
 #include "freertos/FreeRTOS.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
-#include <Led_Light.h>
+// #include <Led_Light.h>
 
 #define LED_PIN GPIO_NUM_18
 
-Led_Light *Led1;
+// Led_Light *Led1;
 
 /* Allow resolution of undecorated (a.k.a. not mangled), C-style references.  */
 extern "C" {
@@ -16,9 +16,9 @@ extern "C" {
 
 void app_main(void) {
     
-    Led1 = new Led_Light(LED_PIN);
-    Led1->blink(1000,500);
+    // Led1 = new Led_Light(LED_PIN);
+    // Led1->blink(1000,500);
     
     // Remove the pointer once it is of any use.
-    delete Led1;
+    // delete Led1;
 } 
