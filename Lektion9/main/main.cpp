0bb85a06e47133896a2f56320b2d075e10061177
Per Thöyrä <per.thoyra@outlook.com>
2024-05-22 17:05:55 +0200

    First light

--- Lektion9/main/main.cpp
+++ Lektion9/main/main.cpp
@@ -3,18 +3,12 @@
 #include "freertos/FreeRTOS.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
-#include <Led_Light.h>
-#include <Button.h>
+#include "driver/adc.h"
+#include <driver/ledc.h>
 
-#define LED_PIN GPIO_NUM_18
-#define BUTTON_PIN GPIO_NUM_27
+#define LED_PIN GPIO_NUM_27
 
 static const char *TAG = "MAIN";
-
-Button *button;
-
-void OnPressed(void* ptr);
-void OnReleased(void* ptr);
 
 /* Allow resolution of undecorated (a.k.a. not mangled), C-style references.  */
 extern "C" 
@@ -24,26 +18,38 @@ extern "C"
 
 void app_main(void) 
 {
-    button = new Button(BUTTON_PIN,PT_down);
+    ledc_timer_config_t timerConfig;
+    timerConfig.clk_cfg = LEDC_AUTO_CLK;
+    timerConfig.duty_resolution = LEDC_TIMER_12_BIT;
+    timerConfig.freq_hz = 100;
+    timerConfig.speed_mode = LEDC_LOW_SPEED_MODE;
+    timerConfig.timer_num = LEDC_TIMER_0;
+    timerConfig.deconfigure = false;
+    ESP_ERROR_CHECK(ledc_timer_config(&timerConfig));
 
-    button->SetOnPressed(OnPressed, (void *)button);
-    button->SetOnRelease(OnReleased, (void *)button);
+    ledc_channel_config_t channelConfig;
+    channelConfig.channel = LEDC_CHANNEL_0;
+    channelConfig.gpio_num = LED_PIN;
+    channelConfig.intr_type = LEDC_INTR_DISABLE;
+    channelConfig.speed_mode = LEDC_LOW_SPEED_MODE;
+    channelConfig.timer_sel = LEDC_TIMER_0;
+
+    channelConfig.duty = 0;
+    channelConfig.hpoint = 0; // Var finns cykelns hig point
+
+    ESP_ERROR_CHECK(ledc_channel_config(&channelConfig));
+
+    uint32_t duty = 0;
 
     for(;;)
     {
-        button->Update();
-        vTaskDelay(pdMS_TO_TICKS(10));
+        ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0b111111);
+        duty += 10;
+        duty = duty % 0b111111111111;
+
+        ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+
+        vTaskDelay(pdMS_TO_TICKS(100));
+        ESP_LOGI(TAG,"Duty %lu", duty);
     }
 } 
-
-void OnPressed(void* ptr)
-{
-    gpio_num_t pin = ((Button *)ptr)->GetPin();
-    ESP_LOGI(TAG,"Button pressed on pin %d", pin);
-}
-
-void OnReleased(void* ptr)
-{
-    gpio_num_t pin = ((Button *)ptr)->GetPin();
-    ESP_LOGI(TAG,"Button released on pin %d", pin);
-}
